1b5263569fa518f721136c16347fea46b34a4923
Bill Paetzke <me@billpaetzke.com>
2013-04-27 20:02:33 -0700

    Add Space model

--- spaces/models.py
+++ spaces/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Space(model.Model):
+
+    name = models.CharField(
+            max_length=63,
+            )
+    phone = models.CharField(
+            max_length=31,
+            )
+    url = models.CharField(
+            max_length=255,
+            )
+    email = models.EmailField()
+
+    def __str__(self):
+
+        return self.name
