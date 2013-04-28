388e1902a3a75c6991c90305795d3d582b139548
Bill Paetzke <me@billpaetzke.com>
2013-04-27 20:45:06 -0700

    Add spaces list view

--- cwb/urls.py
+++ cwb/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
+import spaces.views
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -14,4 +16,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', spaces.views.ListSpaceView.as_view(),
+        name='spaces-list')
 )
--- spaces/views.py
+++ spaces/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.views.generic import ListView
+
+from spaces.models import Space
+
+class ListSpaceView(ListView):
+
+    model = Space # list all spaces in the db
+    template_name = 'space_list.html'
