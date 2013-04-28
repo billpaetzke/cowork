bc8a2811cb740f32ac50c701207075e0a21d8248
Bill Paetzke <me@billpaetzke.com>
2013-04-27 20:29:39 -0700

    Add test case for Space str

--- spaces/tests.py
+++ spaces/tests.py
@@ -1,16 +1,13 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
+from spaces.models import Space
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
+class SpaceTests(TestCase):
+    """Space model tests."""
+    
+    def test_str(self):
+        
+        space = Space(name='WeWork Hollywood')
+
+        self.assertEquals(str(space), 'WeWork Hollywood')
